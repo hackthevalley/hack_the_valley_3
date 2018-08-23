df3ac04371c0e0201012eb48dbe4cfe592dfe011
Jun Zheng <me@jackzh.com>
2018-08-23 17:34:45 -0400

    Add new site

---
+++ new/scripts/app.js
@@ -0,0 +1,16 @@
+(function(){
+    setTimeout(function(){
+        $("#initial-load-screen").fadeOut();
+    }, 1000);
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 20) {
+            $(".main-nav").addClass("scrolled");
+        } else {
+            $(".main-nav").removeClass("scrolled");
+        }
+    }
+
+})();
