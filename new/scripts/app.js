83858c5f06dba0287cb6af8695eb3b29210a2b7e
Jun Zheng <me@jackzh.com>
2018-09-03 19:20:58 -0400

    Ready for prod

--- new/scripts/app.js
+++ new/scripts/app.js
@@ -1,3 +1,11 @@
+$.fn.isInViewport = function() {
+    let elementTop = $(this).offset().top;
+    let elementBottom = elementTop + $(this).outerHeight();
+    let viewportTop = $(window).scrollTop();
+    let viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
 (function(){
     setTimeout(function(){
         $("#initial-load-screen").fadeOut();
@@ -13,8 +21,20 @@
         }
     }
 
-    $('.sponsor-logos .logo-container').tilt({
+    $('.sponsor-logos .logo-container').tilt({});
 
-    })
+    $(window).on('resize scroll', function() {
+        $(".nav-link").removeClass("active");
+        if($("#top").isInViewport()) {
+            $("#top-nav-link").addClass("active");
+        } else if($("#about").isInViewport()){
+            $("#about-nav-link").addClass("active");
+        } else if($("#faqs").isInViewport()){
+            $("#faqs-nav-link").addClass("active");
+        } else if($("#sponsors").isInViewport()){
+            $("#sponsors-nav-link").addClass("active");
+        }
+    });
+    let scroll = new SmoothScroll('a[href*="#"]');
 
 })();
