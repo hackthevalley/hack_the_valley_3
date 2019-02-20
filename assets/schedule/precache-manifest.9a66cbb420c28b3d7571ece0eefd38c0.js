e5d335df23e9758cd6e23916f624216505286c07
Jun Zheng <me@jackzh.com>
2019-02-19 23:39:20 -0500

    publish: Add schedule
    generated from commit cc69d084da48b4837b4a021cc3725059d90d12ed

---
+++ assets/schedule/precache-manifest.9a66cbb420c28b3d7571ece0eefd38c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff78c88ef1f4c9fcfab",
+    "url": "/assets/schedule/static/js/runtime~main.1ff78c88.js"
+  },
+  {
+    "revision": "d2b6e2a683c1273dbe4f",
+    "url": "/assets/schedule/static/js/main.d2b6e2a6.chunk.js"
+  },
+  {
+    "revision": "6307e6d78c1041bfbcb2",
+    "url": "/assets/schedule/static/js/5.6307e6d7.chunk.js"
+  },
+  {
+    "revision": "a5b22ebecf1aae66de73",
+    "url": "/assets/schedule/static/js/4.a5b22ebe.chunk.js"
+  },
+  {
+    "revision": "638a652d548cac079bdc",
+    "url": "/assets/schedule/static/js/3.638a652d.chunk.js"
+  },
+  {
+    "revision": "e097ed42169d73c4c99b",
+    "url": "/assets/schedule/static/js/2.e097ed42.chunk.js"
+  },
+  {
+    "revision": "d2b6e2a683c1273dbe4f",
+    "url": "/assets/schedule/static/css/main.d6726b92.chunk.css"
+  },
+  {
+    "revision": "ea281bd4c585103c2c6297ce99381634",
+    "url": "/assets/schedule/index.html"
+  }
+];
