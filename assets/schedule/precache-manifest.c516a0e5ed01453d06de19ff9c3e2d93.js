725612c0e734db3268f5ac1da6163c2fd7e0dc40
Jun Zheng <me@jackzh.com>
2019-02-22 15:39:13 -0500

    Update schedule to include dinner for tonight :)

---
+++ assets/schedule/precache-manifest.c516a0e5ed01453d06de19ff9c3e2d93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a560668a9d9c749cd552",
+    "url": "/assets/schedule/static/js/runtime~main.a560668a.js"
+  },
+  {
+    "revision": "22aba4daf4a79928bcc0",
+    "url": "/assets/schedule/static/js/main.22aba4da.chunk.js"
+  },
+  {
+    "revision": "87919e808f9b197c5447",
+    "url": "/assets/schedule/static/js/5.87919e80.chunk.js"
+  },
+  {
+    "revision": "260876d1d57ef6258307",
+    "url": "/assets/schedule/static/js/4.260876d1.chunk.js"
+  },
+  {
+    "revision": "e570733f8d86d328eec6",
+    "url": "/assets/schedule/static/js/3.e570733f.chunk.js"
+  },
+  {
+    "revision": "7c7848f551543b130a66",
+    "url": "/assets/schedule/static/js/2.7c7848f5.chunk.js"
+  },
+  {
+    "revision": "22aba4daf4a79928bcc0",
+    "url": "/assets/schedule/static/css/main.9bffeb3c.chunk.css"
+  },
+  {
+    "revision": "d98815e1ee87a8f7c91d7a8a362163f6",
+    "url": "/assets/schedule/index.html"
+  }
+];
