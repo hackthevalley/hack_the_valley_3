9e9849dbf15dceb29fc61f8068d887bf01daffd7
Jun Zheng <me@jackzh.com>
2019-02-24 10:16:10 -0500

    publish: Fix schedule
    generated from commit 0d1d1a1d42608c9a7edc796333af771aba8f85b6

---
+++ assets/schedule/precache-manifest.d9a280c396dfffae6c31ee70c51d7c68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "04d51e56a6e09c33e54a",
+    "url": "/assets/schedule/static/js/runtime~main.04d51e56.js"
+  },
+  {
+    "revision": "22aba4daf4a79928bcc0",
+    "url": "/assets/schedule/static/js/main.22aba4da.chunk.js"
+  },
+  {
+    "revision": "f54cdaec4619555ad7ff",
+    "url": "/assets/schedule/static/js/5.f54cdaec.chunk.js"
+  },
+  {
+    "revision": "603e6b708b12c3fb221c",
+    "url": "/assets/schedule/static/js/4.603e6b70.chunk.js"
+  },
+  {
+    "revision": "dacaa4a5bf69053d2068",
+    "url": "/assets/schedule/static/js/3.dacaa4a5.chunk.js"
+  },
+  {
+    "revision": "7c7848f551543b130a66",
+    "url": "/assets/schedule/static/js/2.7c7848f5.chunk.js"
+  },
+  {
+    "revision": "22aba4daf4a79928bcc0",
+    "url": "/assets/schedule/static/css/main.9bffeb3c.chunk.css"
+  },
+  {
+    "revision": "5e85f3455adcee978b29ca56f57d5a32",
+    "url": "/assets/schedule/index.html"
+  }
+];
